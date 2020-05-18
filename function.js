61421a26c3f267cdd818c901a67b58abd8d1cfdd
Husniye Sekeroglu <husniyesakmak.91@gmail.com>
2020-05-18 15:06:28 +0200

    function.js  modified

--- function.js
+++ function.js
@@ -1,15 +1,15 @@
 function helpAyse() { 
   let numberArray=["sifir","bir","iki","üc","dort","bes","altı","yedi","sekiz","dokuz","on"];
   let value=prompt("Ayse 10 dan Kucuk Bir Sayi Girer misin.");
-  let deger1=value.toLowerCase();
+  let value1=value.toLowerCase();
       for(let i=0;i<11;i++) {
-         if(numberArray[i]==deger1) { 
-         let result=numberArray.indexOf(deger1);
+         if(numberArray[i]==value1) { 
+         let result=numberArray.indexOf(value1);
          console.log(result)
         document.getElementById("result").innerHTML=10-result;
          }
       }
-     if(deger1!=numberArray[0] && deger1!=numberArray[1] && deger1!=numberArray[2] && deger1!=numberArray[3] && deger1!=numberArray[4] && deger1!=numberArray[5] && deger1!=numberArray[6] && deger1!=numberArray[7] && deger1!=numberArray[8] && deger1!=numberArray[9]&& deger1!=numberArray[10]){
+     if(value1!=numberArray[0] && value1!=numberArray[1] && value1!=numberArray[2] && value1!=numberArray[3] && value1!=numberArray[4] && value1!=numberArray[5] && value1!=numberArray[6] && value1!=numberArray[7] && value1!=numberArray[8] && value1!=numberArray[9]&& value1!=numberArray[10]){
        document.getElementById("result").innerHTML="Aysecik sayiyi yanlis yazdin.";
       }
 }
